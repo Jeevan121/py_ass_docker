e3f827e87f7a81e55682108680a18d63e3c9ab58
Jeev Devops <jkborale@gmail.com>
2022-08-14 08:46:32 -0700

    update cors in server

--- api/app.py
+++ api/app.py
@@ -3,10 +3,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
-app.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(app, resources={r"/stats": {"origins": "http://0.0.0.0:5000"}})
+CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route('/stats')
 def stats():
