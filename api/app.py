5c10c9656e51e9edba4f050c0fdee5a3246d5f89
Jeev Devops <jkborale@gmail.com>
2022-08-14 08:32:28 -0700

    updated the app

--- api/app.py
+++ api/app.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app, resources={r"/stats": {"origins": "http://0.0.0.0:5000"}})
 
 @app.route('/stats')
 def stats():
